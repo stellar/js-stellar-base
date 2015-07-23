86dcfbed6c3a2b8b1edcd03ff988f5075ee34589
Frederic Heem <frederic.heem@finclusionsystems.com>
2015-07-23 10:39:46 +0100

    use axios

--- test/functional/functional_test.js
+++ test/functional/functional_test.js
@@ -1,155 +1,140 @@
-var assert = require('assert');
-var Promise = require('bluebird');
+let assert = require('assert');
+let Promise = require('bluebird');
+let axios = require('axios');
 
 describe('Functional test', function() {
   this.timeout(10e3);
-  let url = 'https://horizon-testnet.stellar.org';
+  let baseUrl = 'https://horizon-testnet.stellar.org';
   //let url = 'http://localhost:8000';
-  let supertest = require('supertest')(url);
+  //let kpGateway = StellarBase.Keypair.fromRawSeed("gatewaybestcowinthestable");
 
   let gateway = {
-    public:"gLf9Js8MYGUKAPJLKoDhUcSg8BcHtrnu2KSZkGMvsnQwhMTTVJ"
+    public: "gLf9Js8MYGUKAPJLKoDhUcSg8BcHtrnu2KSZkGMvsnQwhMTTVJ"
   };
+
   let accountAlice;
   let keysAlice = {
-    public:"gsQx3Uofqu2SMHCwzvpeDPjENJF8d9N9GXmnsAQqD6nhMwypbXG",
-    seed:"sfjGjkUHxr5wffg4NPkdFK3fJyT9PjXyu1gAMwHiKdgdXsb3sZ1"
+    public: "gsQx3Uofqu2SMHCwzvpeDPjENJF8d9N9GXmnsAQqD6nhMwypbXG",
+    seed: "sfjGjkUHxr5wffg4NPkdFK3fJyT9PjXyu1gAMwHiKdgdXsb3sZ1"
   };
 
   let keysBob = {
-    public:'gs3uhS4n248kCAVJrAw2eirZt88pnEXUq2sB9AcD3fDMJAwSKbN'
+    public: 'gs3uhS4n248kCAVJrAw2eirZt88pnEXUq2sB9AcD3fDMJAwSKbN'
   };
 
-  function fetchAccount(publicKey, done){
-    return new Promise(function(resolve, reject){
-      supertest.get('/accounts/' + publicKey)
-        .expect(200)
-        .end(function(err, result) {
-          if(err) return reject(err);
-
-          assert(!err);
-          assert(result);
-          assert(result.text);
-          var account = JSON.parse(result.text);
-          assert(account.balances);
-          console.log(JSON.stringify(account, null, 4));
-          return resolve(account);
-        });
-    })
+  function fetchAccount(publicKey) {
+    return axios.get(baseUrl + '/accounts/' + publicKey)
+      .then(function(response) {
+        //console.log("fetchAccount ", response.data)
+        return response.data;
+      })
   }
-
-  before(function(done){
+  before(function(done) {
     fetchAccount(keysAlice.public)
-    .then(function(account){
-      //console.log("seqAlice ", seqAlice);
-      accountAlice = new StellarBase.Account(keysAlice.public, account.sequence);
-    })
-    .then(done, done)
+      .then(function(account) {
+        accountAlice = new StellarBase.Account(keysAlice.public, account.sequence);
+      })
+      .then(done, done)
   })
 
-  it.skip("create account", function(done) {
+  it("create account", function(done) {
     var destination = keysBob.public;
     var startingBalance = 1000;
     let createAccountOp = StellarBase.Operation.createAccount({
-        destination: destination,
-        startingBalance: startingBalance
+      destination: destination,
+      startingBalance: startingBalance
     });
 
     var keyPairAlice = StellarBase.Keypair.fromSeed(keysAlice.seed);
 
     let input = new StellarBase.TransactionBuilder(accountAlice)
-                .addOperation(createAccountOp)
-                .addSigner(keyPairAlice)
-                .build()
-                .toEnvelope()
-                .toXDR('hex');
+      .addOperation(createAccountOp)
+      .addSigner(keyPairAlice)
+      .build()
+      .toEnvelope()
+      .toXDR('hex');
 
-    //console.log(input)
-    supertest.post('/transactions')
-      .set('Content-Type',  'application/x-www-form-urlencoded')
-      .send("tx=" + input)
-      .end(function(err, result) {
-        //assert(!err)
-        //console.log(err);
-        console.log(result.body);
-        //assert.equal(result.body.foo, 'Bar');
-        done();
-      });
-
+    return axios.post(baseUrl + '/transactions', {
+        tx: input
+      })
+      .then(function(response) {
+        console.log(response.data);
+        assert(response.data);
+        assert.equal(response.data.result, 'received');
+      })
+      .then(done, done);
   });
 
   it("alice sends native currency to bob", function(done) {
     let destination = keysBob.public
-    let currency    = StellarBase.Currency.native();
-    let amount      = "5";
+    let currency = StellarBase.Currency.native();
+    let amount = "5";
 
     var keyPairAlice = StellarBase.Keypair.fromSeed(keysAlice.seed);
 
     let input = new StellarBase.TransactionBuilder(accountAlice)
-                .addOperation(StellarBase.Operation.payment({destination, currency, amount}))
-                .addSigner(keyPairAlice)
-                .build()
-                .toEnvelope()
-                .toXDR('hex');
+      .addOperation(StellarBase.Operation.payment({
+        destination, currency, amount
+      }))
+      .addSigner(keyPairAlice)
+      .build()
+      .toEnvelope()
+      .toXDR('hex');
 
-    //console.log(input)
-    supertest.post('/transactions')
-      .set('Content-Type',  'application/x-www-form-urlencoded')
-      .send("tx=" + input)
-      .end(function(err, result) {
-        //assert(!err)
-        //console.log(err);
-        console.log(result.body);
-        var errorXdr = result.body.error;
-        var operation = StellarBase.xdr.TransactionResult.fromXDR(new Buffer(errorXdr, "hex"));
-        console.log(JSON.stringify(operation, null, 4));
-        //assert.equal(result.body.foo, 'Bar');
-        done();
-      });
+    return axios.post(baseUrl + '/transactions', {
+        tx: input
+      })
+      .then(function(response) {
+        //console.log(response.data);
+        assert(response.data);
+        assert.equal(response.data.result, 'received');
+      })
+      .then(done, done)
   });
 
-  it.skip("set trust", function(done) {
+  it("set trust", function(done) {
     let trustor = gateway.public;
     let currencyCode = "USD";
     let authorize = true;
 
     let opTrust = StellarBase.Operation.allowTrust({
-        trustor: trustor,
-        currencyCode: currencyCode,
-        authorize: authorize
+      trustor: trustor,
+      currencyCode: currencyCode,
+      authorize: authorize
     });
 
     var keyPairAlice = StellarBase.Keypair.fromSeed(keysAlice.seed);
 
     let input = new StellarBase.TransactionBuilder(accountAlice)
-                .addSigner(keyPairAlice)
-                .addOperation(opTrust)
-                .build()
-                .toEnvelope()
-                .toXDR('hex');
+      .addSigner(keyPairAlice)
+      .addOperation(opTrust)
+      .build()
+      .toEnvelope()
+      .toXDR('hex');
 
-    supertest.post('/transactions')
-      .send("tx=" + input)
-      .end(function(err, result) {
-        //assert(!err)
-        //console.log(err);
-        console.log(result.body);
-        //assert.equal(result.body.foo, 'Bar');
-        done();
-      });
+    return axios.post(baseUrl + '/transactions', {
+        tx: input
+      })
+      .then(function(response) {
+        //console.log(response.data);
+        assert(response.data);
+        assert.equal(response.data.result, 'received');
+      })
+      .then(done, done)
   });
 
   it("alice sends an invalid transaction", function(done) {
-        let input = 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
+    let input = 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
 
-        supertest.post('/transactions')
-          .send("tx=" + input)
-          .end(function(err, result) {
-            assert(err)
-            assert.equal(err.status, 500);
-            //console.log(err);
-            assert.equal(result.body.type, 'server_error');
-            done();
-          });
-      });
+    return axios.post(baseUrl + '/transactions', {
+        tx: input
+      })
+      .catch(function(error) {
+        //console.log(response.data);
+        assert(error);
+        //assert.equal(response.data.result, 'received');
+      })
+      .then(done, done)
+
+  });
 });
