22abf428bae8c5ef0281ad492e5df29f110f7e37
Bartek Nowotarski <bartek@nowotarski.info>
2015-08-24 17:37:34 +0200

    Building in travis script and bower_publish script debug

--- bower_publish.sh
+++ bower_publish.sh
@@ -2,16 +2,26 @@
 
 git clone "https://stellar-jenkins@github.com/stellar/bower-js-stellar-base.git" bower
 
+echo 0
+
 if [ ! -d "bower" ]; then
   echo "Error cloning"
   exit 1
 fi
 
 cp dist/* bower
+echo 1
 cd bower
+echo 2
 git add .
+echo 3
 git commit -m $TRAVIS_TAG
+echo 4
 git tag -a $TRAVIS_TAG -m $TRAVIS_TAG
+echo 5
 git push origin master --tags
+echo 6
 cd ..
+echo 7
 rm -rf bower
+echo 8
