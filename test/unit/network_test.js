a94df2773a74aa2f3b38b890902d2977a41c8318
Bartek Nowotarski <bartek@nowotarski.info>
2015-09-11 17:09:51 +0200

    Test

--- test/unit/network_test.js
+++ test/unit/network_test.js
@@ -7,3 +7,16 @@ describe("Network.current()", function() {
   })
 
 });
+
+describe("Network.usePublicNetwork()", function() {
+
+  beforeEach(function() {
+    StellarBase.Network.useDefault();
+  });
+
+  it("switches to the public network", function() {
+    StellarBase.Network.usePublicNetwork();
+    expect(StellarBase.Network.current().networkPassphrase()).to.equal(StellarBase.Networks.PUBLIC)
+  });
+
+});
