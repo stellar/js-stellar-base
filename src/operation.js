fc5f89e36eb13edf5a15eb1f001b3f9e5a1fbfe5
Bartek Nowotarski <bartek@nowotarski.info>
2016-01-28 18:12:29 +0100

    Update jsdoc

--- src/operation.js
+++ src/operation.js
@@ -308,8 +308,8 @@ export class Operation {
     * @param {Asset} opts.buying - What you're buying.
     * @param {string} opts.amount - The total amount you're selling. If 0, deletes the offer.
     * @param {number|string|BigNumber|Object} opts.price - The exchange rate ratio (selling / buying)
-    * @param {number} opts.price.n - The price numerator
-    * @param {number} opts.price.d - The price denominator
+    * @param {number} opts.price.n - If `opts.price` is an object: the price numerator
+    * @param {number} opts.price.d - If `opts.price` is an object: the price denominator
     * @param {number|string} [opts.offerId ]- If `0`, will create a new offer (default). Otherwise, edits an exisiting offer.
     * @param {string} [opts.source] - The source account (defaults to transaction source).
     * @throws {Error} Throws `Error` when the best rational approximation of `price` cannot be found.
@@ -353,8 +353,8 @@ export class Operation {
     * @param {Asset} opts.buying - What you're buying.
     * @param {string} opts.amount - The total amount you're selling. If 0, deletes the offer.
     * @param {number|string|BigNumber|Object} opts.price - The exchange rate ratio (selling / buying)
-    * @param {number} opts.price.n - The price numerator
-    * @param {number} opts.price.d - The price denominator
+    * @param {number} opts.price.n - If `opts.price` is an object: the price numerator
+    * @param {number} opts.price.d - If `opts.price` is an object: the price denominator
     * @param {string} [opts.source] - The source account (defaults to transaction source).
     * @throws {Error} Throws `Error` when the best rational approximation of `price` cannot be found.
     * @returns {xdr.CreatePassiveOfferOp}
