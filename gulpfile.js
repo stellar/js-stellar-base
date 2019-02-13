25178eb9dfcb96bec0739edb2e0760e058ef7442
Morley Zhi <morley@lightyear.io>
2019-02-13 17:50:13 -0500

    ugrade packages, port to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,10 @@
 var gulp = require('gulp');
 var isparta = require('isparta');
 var plugins = require('gulp-load-plugins')();
-var runSequence = require('run-sequence');
 var clear = require('clear');
 var webpack = require('webpack');
-var plumber = require('gulp-plumber');
 
-gulp.task('default', ['build']);
-
-gulp.task('lint:src', function() {
+gulp.task('lint:src', function lintSrc() {
   return gulp
     .src(['src/**/*.js'])
     .pipe(plugins.eslint())
@@ -19,7 +15,7 @@ gulp.task('lint:src', function() {
 });
 
 // Lint our test code
-gulp.task('lint:test', function() {
+gulp.task('lint:test', function lintTest() {
   return gulp
     .src(['test/unit/**/*.js'])
     .pipe(plugins.eslint())
@@ -27,110 +23,146 @@ gulp.task('lint:test', function() {
     .pipe(plugins.eslint.failAfterError());
 });
 
-gulp.task('build', function(done) {
-  runSequence('clean', 'build:node', 'build:browser', done);
-});
+gulp.task(
+  'build:node',
+  gulp.series('lint:src', function buildNode() {
+    return gulp
+      .src('src/**/*.js')
+      .pipe(plugins.babel())
+      .pipe(gulp.dest('lib'));
+  })
+);
 
-gulp.task('test', function(done) {
-  runSequence('clean', 'test:node', 'test:browser', done);
-});
-
-gulp.task('hooks:precommit', ['build'], function() {
-  return gulp.src(['dist/*', 'lib/*']).pipe(plugins.git.add());
-});
-
-gulp.task('build:node', ['lint:src'], function() {
-  return gulp
-    .src('src/**/*.js')
-    .pipe(plugins.babel())
-    .pipe(gulp.dest('lib'));
-});
-
-gulp.task('build:browser', ['lint:src'], function() {
-  return gulp
-    .src('src/browser.js')
-    .pipe(
-      plugins.webpack({
-        output: { library: 'StellarBase' },
-        module: {
-          loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+gulp.task(
+  'build:browser',
+  gulp.series('lint:src', function buildNode() {
+    return gulp
+      .src('src/browser.js')
+      .pipe(
+        plugins.webpack({
+          output: { library: 'StellarBase' },
+          module: {
+            loaders: [
+              { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+            ]
+          },
+          plugins: [
+            // Ignore native modules (ed25519)
+            new webpack.IgnorePlugin(/ed25519/)
           ]
-        },
-        plugins: [
-          // Ignore native modules (ed25519)
-          new webpack.IgnorePlugin(/ed25519/)
-        ]
-      })
-    )
-    .pipe(plugins.rename('stellar-base.js'))
-    .pipe(gulp.dest('dist'))
-    .pipe(
-      plugins.uglify({
-        output: {
-          ascii_only: true
+        })
+      )
+      .pipe(plugins.rename('stellar-base.js'))
+      .pipe(gulp.dest('dist'))
+      .pipe(
+        plugins.uglify({
+          output: {
+            ascii_only: true
+          }
+        })
+      )
+      .pipe(plugins.rename('stellar-base.min.js'))
+      .pipe(gulp.dest('dist'));
+  })
+);
+
+gulp.task('clean-coverage', function cleanCoverage() {
+  return gulp.src(['coverage'], { read: false }).pipe(plugins.rimraf());
+});
+
+gulp.task(
+  'test:init-istanbul',
+  gulp.series('clean-coverage', function testInitIstanbul() {
+    return gulp
+      .src(['src/**/*.js'])
+      .pipe(
+        plugins.istanbul({
+          instrumenter: isparta.Instrumenter
+        })
+      )
+      .pipe(plugins.istanbul.hookRequire());
+  })
+);
+
+gulp.task(
+  'test:node',
+  gulp.series('build:node', 'test:init-istanbul', function testNode() {
+    return gulp
+      .src(['test/test-helper.js', 'test/unit/**/*.js'])
+      .pipe(
+        plugins.mocha({
+          reporter: ['dot']
+        })
+      )
+      .pipe(plugins.istanbul.writeReports());
+  })
+);
+
+gulp.task(
+  'test:browser',
+  gulp.series('build:browser', function testBrowser(done) {
+    var Server = require('karma').Server;
+    var server = new Server(
+      { configFile: __dirname + '/karma.conf.js' },
+      (exitCode) => {
+        if (exitCode !== 0) {
+          done(new Error(`Bad exit code ${exitCode}`));
+        } else {
+          done();
         }
-      })
-    )
-    .pipe(plugins.rename('stellar-base.min.js'))
-    .pipe(gulp.dest('dist'));
-});
+      }
+    );
+    server.start();
+  })
+);
 
-gulp.task('test:init-istanbul', ['clean-coverage'], function() {
-  return gulp
-    .src(['src/**/*.js'])
-    .pipe(
-      plugins.istanbul({
-        instrumenter: isparta.Instrumenter
-      })
-    )
-    .pipe(plugins.istanbul.hookRequire());
-});
+gulp.task(
+  'test:sauce',
+  gulp.series('build:browser', function testSauce(done) {
+    var Server = require('karma').Server;
+    var server = new Server(
+      { configFile: __dirname + '/karma-sauce.conf.js' },
+      (exitCode) => {
+        if (exitCode !== 0) {
+          done(new Error(`Bad exit code ${exitCode}`));
+        } else {
+          done();
+        }
+      }
+    );
+    server.start();
+  })
+);
 
-gulp.task('test:node', ['build:node', 'test:init-istanbul'], function() {
-  return gulp
-    .src(['test/test-helper.js', 'test/unit/**/*.js'])
-    .pipe(
-      plugins.mocha({
-        reporter: ['dot']
-      })
-    )
-    .pipe(plugins.istanbul.writeReports());
-});
-
-gulp.task('test:browser', ['build:browser'], function(done) {
-  var Server = require('karma').Server;
-  var server = new Server({ configFile: __dirname + '/karma.conf.js' });
-  server.start(function() {
-    done();
-  });
-});
-
-gulp.task('test:sauce', ['build:browser'], function(done) {
-  var Server = require('karma').Server;
-  var server = new Server({ configFile: __dirname + '/karma-sauce.conf.js' });
-  server.start(function() {
-    done();
-  });
-});
-
-gulp.task('clear-screen', function(cb) {
+gulp.task('clear-screen', function clearScreen(cb) {
   clear();
   cb();
 });
 
-gulp.task('clean', function() {
+gulp.task('clean', function clean() {
   return gulp.src(['dist', 'lib'], { read: false }).pipe(plugins.rimraf());
 });
 
-gulp.task('watch', ['build'], function() {
-  gulp.watch('src/**/*', ['clear-screen', 'build']);
-});
+gulp.task('build', gulp.series('clean', 'build:node', 'build:browser'));
 
-gulp.task('clean-coverage', function() {
-  return gulp.src(['coverage'], { read: false }).pipe(plugins.rimraf());
-});
+gulp.task('test', gulp.series('clean', 'test:node', 'test:browser'));
 
-gulp.task('submit-coverage', function() {
+gulp.task('default', gulp.series('build'));
+
+gulp.task(
+  'hooks:precommit',
+  gulp.series('build', function hooksPrecommit() {
+    return gulp.src(['dist/*', 'lib/*']).pipe(plugins.git.add());
+  })
+);
+
+gulp.task(
+  'watch',
+  gulp.series('build', function watch() {
+    gulp.watch('src/**/*', ['clear-screen', 'build']);
+  })
+);
+
+gulp.task('submit-coverage', function submitCoverage() {
   return gulp.src('./coverage/**/lcov.info').pipe(plugins.coveralls());
 });
