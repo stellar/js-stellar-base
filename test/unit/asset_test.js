125033aea85ab032964c8155567b63fdd0ed21b4
Morley Zhi <morley@lightyear.io>
2019-02-19 12:15:58 -0500

    try to restore tests again

--- test/unit/asset_test.js
+++ test/unit/asset_test.js
@@ -1,208 +1,154 @@
 describe('Asset', function() {
-  describe('constructor', function() {
-    it("throws an error when there's no issuer for non XLM type asset", function() {
-      expect(() => new StellarBase.Asset('USD')).to.throw(
-        /Issuer cannot be null/
-      );
+
+    describe("constructor", function () {
+
+        it("throws an error when there's no issuer for non XLM type asset", function () {
+            expect(() => new StellarBase.Asset("USD")).to.throw(/Issuer cannot be null/)
+        });
+
+        it("throws an error when code is invalid", function () {
+            expect(() => new StellarBase.Asset("", "GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ")).to.throw(/Asset code is invalid/)
+            expect(() => new StellarBase.Asset("1234567890123", "GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ")).to.throw(/Asset code is invalid/)
+            expect(() => new StellarBase.Asset("ab_", "GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ")).to.throw(/Asset code is invalid/)
+        });
+
+        it("throws an error when issuer is invalid", function () {
+            expect(() => new StellarBase.Asset("USD", "GCEZWKCA5")).to.throw(/Issuer is invalid/)
+        });
     });
 
-    it('throws an error when code is invalid', function() {
-      expect(
-        () =>
-          new StellarBase.Asset(
-            '',
-            'GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ'
-          )
-      ).to.throw(/Asset code is invalid/);
-      expect(
-        () =>
-          new StellarBase.Asset(
-            '1234567890123',
-            'GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ'
-          )
-      ).to.throw(/Asset code is invalid/);
-      expect(
-        () =>
-          new StellarBase.Asset(
-            'ab_',
-            'GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ'
-          )
-      ).to.throw(/Asset code is invalid/);
+    describe("getCode()", function () {
+        it("returns a code for a native asset object", function () {
+            var asset = new StellarBase.Asset.native();
+            expect(asset.getCode()).to.be.equal('XLM');
+        });
+
+        it("returns a code for a non-native asset", function () {
+            var asset = new StellarBase.Asset("USD", "GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ");
+            expect(asset.getCode()).to.be.equal('USD');
+        });
     });
 
-    it('throws an error when issuer is invalid', function() {
-      expect(() => new StellarBase.Asset('USD', 'GCEZWKCA5')).to.throw(
-        /Issuer is invalid/
-      );
-    });
-  });
+    describe("getIssuer()", function () {
+        it("returns a code for a native asset object", function () {
+            var asset = new StellarBase.Asset.native();
+            expect(asset.getIssuer()).to.be.undefined;
+        });
 
-  describe('getCode()', function() {
-    it('returns a code for a native asset object', function() {
-      var asset = new StellarBase.Asset.native();
-      expect(asset.getCode()).to.be.equal('XLM');
-    });
-
-    it('returns a code for a non-native asset', function() {
-      var asset = new StellarBase.Asset(
-        'USD',
-        'GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ'
-      );
-      expect(asset.getCode()).to.be.equal('USD');
-    });
-  });
-
-  describe('getIssuer()', function() {
-    it('returns a code for a native asset object', function() {
-      var asset = new StellarBase.Asset.native();
-      expect(asset.getIssuer()).to.be.undefined;
+        it("returns a code for a non-native asset", function () {
+            var asset = new StellarBase.Asset("USD", "GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ");
+            expect(asset.getIssuer()).to.be.equal('GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ');
+        });
     });
 
-    it('returns a code for a non-native asset', function() {
-      var asset = new StellarBase.Asset(
-        'USD',
-        'GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ'
-      );
-      expect(asset.getIssuer()).to.be.equal(
-        'GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ'
-      );
-    });
-  });
+    describe("getAssetType()", function () {
+        it("returns native for native assets", function () {
+            var asset = StellarBase.Asset.native();
+            expect(asset.getAssetType()).to.eq("native");
+        });
 
-  describe('getAssetType()', function() {
-    it('returns native for native assets', function() {
-      var asset = StellarBase.Asset.native();
-      expect(asset.getAssetType()).to.eq('native');
-    });
+        it("returns credit_alphanum4 if the asset code length is between 1 and 4", function () {
+            var asset = new StellarBase.Asset("ABCD", "GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ");
+            expect(asset.getAssetType()).to.eq("credit_alphanum4");
+        });
 
-    it('returns credit_alphanum4 if the asset code length is between 1 and 4', function() {
-      var asset = new StellarBase.Asset(
-        'ABCD',
-        'GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ'
-      );
-      expect(asset.getAssetType()).to.eq('credit_alphanum4');
+        it("returns credit_alphanum12 if the asset code length is between 5 and 12", function () {
+            var asset = new StellarBase.Asset("ABCDEF", "GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ");
+            expect(asset.getAssetType()).to.eq("credit_alphanum12");
+        });
     });
 
-    it('returns credit_alphanum12 if the asset code length is between 5 and 12', function() {
-      var asset = new StellarBase.Asset(
-        'ABCDEF',
-        'GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ'
-      );
-      expect(asset.getAssetType()).to.eq('credit_alphanum12');
-    });
-  });
+    describe("toXDRObject()", function () {
+        it("parses a native asset object", function () {
+            var asset = new StellarBase.Asset.native();
+            var xdr = asset.toXDRObject();
+            expect(xdr.toXDR().toString()).to.be.equal(Buffer.from([0,0,0,0]).toString());
+        });
 
-  describe('toXDRObject()', function() {
-    it('parses a native asset object', function() {
-      var asset = new StellarBase.Asset.native();
-      var xdr = asset.toXDRObject();
-      expect(xdr.toXDR().toString()).to.be.equal(
-        Buffer.from([0, 0, 0, 0]).toString()
-      );
-    });
+        it("parses a 3-alphanum asset object", function () {
+            var asset = new StellarBase.Asset("USD", "GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ");
+            var xdr = asset.toXDRObject();
 
-    it('parses a 3-alphanum asset object', function() {
-      var asset = new StellarBase.Asset(
-        'USD',
-        'GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ'
-      );
-      var xdr = asset.toXDRObject();
+            expect(xdr).to.be.instanceof(StellarBase.xdr.Asset);
+            expect(() => xdr.toXDR('hex')).to.not.throw();
 
-      expect(xdr).to.be.instanceof(StellarBase.xdr.Asset);
-      expect(() => xdr.toXDR('hex')).to.not.throw();
+            expect(xdr.arm()).to.equal('alphaNum4');
+            expect(xdr.value().assetCode()).to.equal('USD\0');
+        });
 
-      expect(xdr.arm()).to.equal('alphaNum4');
-      expect(xdr.value().assetCode()).to.equal('USD\0');
-    });
+        it("parses a 4-alphanum asset object", function () {
+            var asset = new StellarBase.Asset("BART", "GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ");
+            var xdr = asset.toXDRObject();
 
-    it('parses a 4-alphanum asset object', function() {
-      var asset = new StellarBase.Asset(
-        'BART',
-        'GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ'
-      );
-      var xdr = asset.toXDRObject();
+            expect(xdr).to.be.instanceof(StellarBase.xdr.Asset);
+            expect(() => xdr.toXDR('hex')).to.not.throw();
 
-      expect(xdr).to.be.instanceof(StellarBase.xdr.Asset);
-      expect(() => xdr.toXDR('hex')).to.not.throw();
+            expect(xdr.arm()).to.equal('alphaNum4');
+            expect(xdr.value().assetCode()).to.equal('BART');
+        });
 
-      expect(xdr.arm()).to.equal('alphaNum4');
-      expect(xdr.value().assetCode()).to.equal('BART');
-    });
+        it("parses a 5-alphanum asset object", function () {
+            var asset = new StellarBase.Asset("12345", "GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ");
+            var xdr = asset.toXDRObject();
 
-    it('parses a 5-alphanum asset object', function() {
-      var asset = new StellarBase.Asset(
-        '12345',
-        'GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ'
-      );
-      var xdr = asset.toXDRObject();
+            expect(xdr).to.be.instanceof(StellarBase.xdr.Asset);
+            expect(() => xdr.toXDR('hex')).to.not.throw();
 
-      expect(xdr).to.be.instanceof(StellarBase.xdr.Asset);
-      expect(() => xdr.toXDR('hex')).to.not.throw();
+            expect(xdr.arm()).to.equal('alphaNum12');
+            expect(xdr.value().assetCode()).to.equal('12345\0\0\0\0\0\0\0');
+        });
 
-      expect(xdr.arm()).to.equal('alphaNum12');
-      expect(xdr.value().assetCode()).to.equal('12345\0\0\0\0\0\0\0');
+        it("parses a 12-alphanum asset object", function () {
+            var asset = new StellarBase.Asset("123456789012", "GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ");
+            var xdr = asset.toXDRObject();
+
+            expect(xdr).to.be.instanceof(StellarBase.xdr.Asset);
+            expect(() => xdr.toXDR('hex')).to.not.throw();
+
+            expect(xdr.arm()).to.equal('alphaNum12');
+            expect(xdr.value().assetCode()).to.equal('123456789012');
+        });
     });
 
-    it('parses a 12-alphanum asset object', function() {
-      var asset = new StellarBase.Asset(
-        '123456789012',
-        'GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ'
-      );
-      var xdr = asset.toXDRObject();
+    describe("fromOperation()", function () {
+      it("parses a native asset XDR", function () {
+        var xdr = new StellarBase.xdr.Asset.assetTypeNative();
+        var asset = StellarBase.Asset.fromOperation(xdr);
 
-      expect(xdr).to.be.instanceof(StellarBase.xdr.Asset);
-      expect(() => xdr.toXDR('hex')).to.not.throw();
-
-      expect(xdr.arm()).to.equal('alphaNum12');
-      expect(xdr.value().assetCode()).to.equal('123456789012');
-    });
-  });
-
-  describe('fromOperation()', function() {
-    it('parses a native asset XDR', function() {
-      var xdr = new StellarBase.xdr.Asset.assetTypeNative();
-      var asset = StellarBase.Asset.fromOperation(xdr);
-
-      expect(asset).to.be.instanceof(StellarBase.Asset);
-      expect(asset.isNative()).to.equal(true);
-    });
-
-    it('parses a 4-alphanum asset XDR', function() {
-      var issuer = 'GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ';
-      var assetCode = 'KHL';
-      var assetType = new StellarBase.xdr.AssetAlphaNum4({
-        assetCode: assetCode + '\0',
-        issuer: StellarBase.Keypair.fromPublicKey(issuer).xdrAccountId()
+        expect(asset).to.be.instanceof(StellarBase.Asset);
+        expect(asset.isNative()).to.equal(true);
       });
-      var xdr = new StellarBase.xdr.Asset(
-        'assetTypeCreditAlphanum4',
-        assetType
-      );
 
-      var asset = StellarBase.Asset.fromOperation(xdr);
+      it("parses a 4-alphanum asset XDR", function () {
+        var issuer = 'GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ'
+        var assetCode = 'KHL';
+        var assetType = new StellarBase.xdr.AssetAlphaNum4({
+          assetCode: assetCode + '\0',
+          issuer: StellarBase.Keypair.fromPublicKey(issuer).xdrAccountId()
+        });
+        var xdr = new StellarBase.xdr.Asset('assetTypeCreditAlphanum4', assetType);
 
-      expect(asset).to.be.instanceof(StellarBase.Asset);
-      expect(asset.getCode()).to.equal(assetCode);
-      expect(asset.getIssuer()).to.equal(issuer);
-    });
+        var asset = StellarBase.Asset.fromOperation(xdr);
 
-    it('parses a 12-alphanum asset XDR', function() {
-      var issuer = 'GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ';
-      var assetCode = 'KHLTOKEN';
-      var assetType = new StellarBase.xdr.AssetAlphaNum4({
-        assetCode: assetCode + '\0\0\0\0',
-        issuer: StellarBase.Keypair.fromPublicKey(issuer).xdrAccountId()
+        expect(asset).to.be.instanceof(StellarBase.Asset);
+        expect(asset.getCode()).to.equal(assetCode);
+        expect(asset.getIssuer()).to.equal(issuer);
       });
-      var xdr = new StellarBase.xdr.Asset(
-        'assetTypeCreditAlphanum12',
-        assetType
-      );
 
-      var asset = StellarBase.Asset.fromOperation(xdr);
+      it("parses a 12-alphanum asset XDR", function () {
+        var issuer = 'GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ'
+        var assetCode = 'KHLTOKEN';
+        var assetType = new StellarBase.xdr.AssetAlphaNum4({
+          assetCode: assetCode + '\0\0\0\0',
+          issuer: StellarBase.Keypair.fromPublicKey(issuer).xdrAccountId()
+        });
+        var xdr = new StellarBase.xdr.Asset('assetTypeCreditAlphanum12', assetType);
 
-      expect(asset).to.be.instanceof(StellarBase.Asset);
-      expect(asset.getCode()).to.equal(assetCode);
-      expect(asset.getIssuer()).to.equal(issuer);
+        var asset = StellarBase.Asset.fromOperation(xdr);
+
+        expect(asset).to.be.instanceof(StellarBase.Asset);
+        expect(asset.getCode()).to.equal(assetCode);
+        expect(asset.getIssuer()).to.equal(issuer);
+      });
     });
-  });
 });
--- test/unit/memo_test.js
+++ test/unit/memo_test.js
@@ -1,11 +1,12 @@
-describe('Memo.constructor()', function() {
-  it('throws error when type is invalid', function() {
-    expect(() => new StellarBase.Memo('test')).to.throw(/Invalid memo type/);
+
+describe("Memo.constructor()", function() {
+  it("throws error when type is invalid", function() {
+    expect(() => new StellarBase.Memo("test")).to.throw(/Invalid memo type/);
   });
 });
 
-describe('Memo.none()', function() {
-  it('converts to/from xdr object', function() {
+describe("Memo.none()", function() {
+  it("converts to/from xdr object", function() {
     let memo = StellarBase.Memo.none().toXDRObject();
     expect(memo.value()).to.be.undefined;
 
@@ -15,47 +16,35 @@ describe('Memo.none()', function() {
   });
 });
 
-describe('Memo.text()', function() {
-  it('returns a value for a correct argument', function() {
-    expect(() => StellarBase.Memo.text('test')).to.not.throw();
-    let memoUtf8 = StellarBase.Memo.text('三代之時');
+describe("Memo.text()", function() {
 
-    let a = Buffer.from(memoUtf8.toXDRObject().value(), 'utf8');
-    let b = Buffer.from('三代之時', 'utf8');
+  it("returns a value for a correct argument", function() {
+    expect(() => StellarBase.Memo.text("test")).to.not.throw();
+    let memoUtf8 = StellarBase.Memo.text("三代之時")
+
+    let a = Buffer.from(memoUtf8.toXDRObject().value(), "utf8");
+    let b = Buffer.from("三代之時", "utf8");
     expect(a).to.be.deep.equal(b);
   });
 
-  it('returns a value for a correct argument (utf8)', function() {
-    let memoText = StellarBase.Memo.text([0xd1])
-      .toXDRObject()
-      .toXDR();
+  it("returns a value for a correct argument (utf8)", function() {
+    let memoText = StellarBase.Memo.text([0xd1]).toXDRObject().toXDR();
     let expected = Buffer.from([
       // memo_text
-      0x00,
-      0x00,
-      0x00,
-      0x01,
+      0x00, 0x00, 0x00, 0x01,
       // length
-      0x00,
-      0x00,
-      0x00,
-      0x01,
+      0x00, 0x00, 0x00, 0x01,
       // value
-      0xd1,
-      0x00,
-      0x00,
-      0x00
+      0xd1, 0x00, 0x00, 0x00
     ]);
     expect(memoText.equals(expected)).to.be.true;
 
-    memoText = StellarBase.Memo.text(Buffer.from([0xd1]))
-      .toXDRObject()
-      .toXDR();
+    memoText = StellarBase.Memo.text(Buffer.from([0xd1])).toXDRObject().toXDR();
     expect(memoText.equals(expected)).to.be.true;
   });
 
-  it('converts to/from xdr object', function() {
-    let memo = StellarBase.Memo.text('test').toXDRObject();
+  it("converts to/from xdr object", function() {
+    let memo = StellarBase.Memo.text("test").toXDRObject();
     expect(memo.arm()).to.equal('text');
     expect(memo.text()).to.equal('test');
     expect(memo.value()).to.equal('test');
@@ -65,7 +54,7 @@ describe('Memo.text()', function() {
     expect(baseMemo.value).to.be.equal('test');
   });
 
-  it('converts to/from xdr object (array)', function() {
+  it("converts to/from xdr object (array)", function() {
     let memo = StellarBase.Memo.text([0xd1]).toXDRObject();
     expect(memo.arm()).to.equal('text');
 
@@ -77,7 +66,7 @@ describe('Memo.text()', function() {
     expect(baseMemo.value).to.be.deep.equal([0xd1]);
   });
 
-  it('converts to/from xdr object (buffer)', function() {
+  it("converts to/from xdr object (buffer)", function() {
     let buf = Buffer.from([0xd1]);
     let memo = StellarBase.Memo.text(buf).toXDRObject();
     expect(memo.arm()).to.equal('text');
@@ -90,42 +79,29 @@ describe('Memo.text()', function() {
     expect(baseMemo.value.equals(buf)).to.be.true;
   });
 
-  it('throws an error when invalid argument was passed', function() {
-    expect(() => StellarBase.Memo.text()).to.throw(
-      /Expects string, array or buffer, max 28 bytes/
-    );
-    expect(() => StellarBase.Memo.text({})).to.throw(
-      /Expects string, array or buffer, max 28 bytes/
-    );
-    expect(() => StellarBase.Memo.text(10)).to.throw(
-      /Expects string, array or buffer, max 28 bytes/
-    );
-    expect(() => StellarBase.Memo.text(Infinity)).to.throw(
-      /Expects string, array or buffer, max 28 bytes/
-    );
-    expect(() => StellarBase.Memo.text(NaN)).to.throw(
-      /Expects string, array or buffer, max 28 bytes/
-    );
+  it("throws an error when invalid argument was passed", function() {
+    expect(() => StellarBase.Memo.text()).to.throw(/Expects string, array or buffer, max 28 bytes/);
+    expect(() => StellarBase.Memo.text({})).to.throw(/Expects string, array or buffer, max 28 bytes/);
+    expect(() => StellarBase.Memo.text(10)).to.throw(/Expects string, array or buffer, max 28 bytes/);
+    expect(() => StellarBase.Memo.text(Infinity)).to.throw(/Expects string, array or buffer, max 28 bytes/);
+    expect(() => StellarBase.Memo.text(NaN)).to.throw(/Expects string, array or buffer, max 28 bytes/);
   });
 
-  it('throws an error when string is longer than 28 bytes', function() {
-    expect(() =>
-      StellarBase.Memo.text('12345678901234567890123456789')
-    ).to.throw(/Expects string, array or buffer, max 28 bytes/);
-    expect(() => StellarBase.Memo.text('三代之時三代之時三代之時')).to.throw(
-      /Expects string, array or buffer, max 28 bytes/
-    );
+  it("throws an error when string is longer than 28 bytes", function() {
+    expect(() => StellarBase.Memo.text("12345678901234567890123456789")).to.throw(/Expects string, array or buffer, max 28 bytes/);
+    expect(() => StellarBase.Memo.text("三代之時三代之時三代之時")).to.throw(/Expects string, array or buffer, max 28 bytes/);
   });
+
 });
 
-describe('Memo.id()', function() {
-  it('returns a value for a correct argument', function() {
-    expect(() => StellarBase.Memo.id('1000')).to.not.throw();
-    expect(() => StellarBase.Memo.id('0')).to.not.throw();
+describe("Memo.id()", function() {
+  it("returns a value for a correct argument", function() {
+    expect(() => StellarBase.Memo.id("1000")).to.not.throw();
+    expect(() => StellarBase.Memo.id("0")).to.not.throw();
   });
 
-  it('converts to/from xdr object', function() {
-    let memo = StellarBase.Memo.id('1000').toXDRObject();
+  it("converts to/from xdr object", function() {
+    let memo = StellarBase.Memo.id("1000").toXDRObject();
     expect(memo.arm()).to.equal('id');
     expect(memo.id().toString()).to.equal('1000');
 
@@ -134,17 +110,17 @@ describe('Memo.id()', function() {
     expect(baseMemo.value).to.be.equal('1000');
   });
 
-  it('throws an error when invalid argument was passed', function() {
+  it("throws an error when invalid argument was passed", function() {
     expect(() => StellarBase.Memo.id()).to.throw(/Expects a int64/);
     expect(() => StellarBase.Memo.id({})).to.throw(/Expects a int64/);
     expect(() => StellarBase.Memo.id(Infinity)).to.throw(/Expects a int64/);
     expect(() => StellarBase.Memo.id(NaN)).to.throw(/Expects a int64/);
-    expect(() => StellarBase.Memo.id('test')).to.throw(/Expects a int64/);
+    expect(() => StellarBase.Memo.id("test")).to.throw(/Expects a int64/);
   });
 });
 
-describe('Memo.hash() & Memo.return()', function() {
-  it('hash converts to/from xdr object', function() {
+describe("Memo.hash() & Memo.return()", function() {
+  it("hash converts to/from xdr object", function() {
     let buffer = Buffer.alloc(32, 10);
 
     let memo = StellarBase.Memo.hash(buffer).toXDRObject();
@@ -158,11 +134,11 @@ describe('Memo.hash() & Memo.return()', function() {
     expect(baseMemo.value.toString('hex')).to.be.equal(buffer.toString('hex'));
   });
 
-  it('return converts to/from xdr object', function() {
+  it("return converts to/from xdr object", function() {
     let buffer = Buffer.alloc(32, 10);
 
     // Testing string hash
-    let memo = StellarBase.Memo.return(buffer.toString('hex')).toXDRObject();
+    let memo = StellarBase.Memo.return(buffer.toString("hex")).toXDRObject();
     expect(memo.arm()).to.equal('retHash');
     expect(memo.retHash().length).to.equal(32);
     expect(memo.retHash().toString('hex')).to.equal(buffer.toString('hex'));
@@ -176,40 +152,26 @@ describe('Memo.hash() & Memo.return()', function() {
 
   var methods = [StellarBase.Memo.hash, StellarBase.Memo.return];
 
-  it('returns a value for a correct argument', function() {
+  it("returns a value for a correct argument", function() {
     for (let i in methods) {
       let method = methods[i];
       expect(() => method(Buffer.alloc(32))).to.not.throw();
-      expect(() =>
-        method(
-          '0000000000000000000000000000000000000000000000000000000000000000'
-        )
-      ).to.not.throw();
+      expect(() => method('0000000000000000000000000000000000000000000000000000000000000000')).to.not.throw();
     }
   });
 
-  it('throws an error when invalid argument was passed', function() {
+  it("throws an error when invalid argument was passed", function() {
     for (let i in methods) {
       let method = methods[i];
       expect(() => method()).to.throw(/Expects a 32 byte hash value/);
       expect(() => method({})).to.throw(/Expects a 32 byte hash value/);
       expect(() => method(Infinity)).to.throw(/Expects a 32 byte hash value/);
       expect(() => method(NaN)).to.throw(/Expects a 32 byte hash value/);
-      expect(() => method('test')).to.throw(/Expects a 32 byte hash value/);
-      expect(() => method([0, 10, 20])).to.throw(
-        /Expects a 32 byte hash value/
-      );
-      expect(() => method(Buffer.alloc(33))).to.throw(
-        /Expects a 32 byte hash value/
-      );
-      expect(() =>
-        method('00000000000000000000000000000000000000000000000000000000000000')
-      ).to.throw(/Expects a 32 byte hash value/);
-      expect(() =>
-        method(
-          '000000000000000000000000000000000000000000000000000000000000000000'
-        )
-      ).to.throw(/Expects a 32 byte hash value/);
+      expect(() => method("test")).to.throw(/Expects a 32 byte hash value/);
+      expect(() => method([0, 10, 20])).to.throw(/Expects a 32 byte hash value/);
+      expect(() => method(Buffer.alloc(33))).to.throw(/Expects a 32 byte hash value/);
+      expect(() => method('00000000000000000000000000000000000000000000000000000000000000')).to.throw(/Expects a 32 byte hash value/);
+      expect(() => method('000000000000000000000000000000000000000000000000000000000000000000')).to.throw(/Expects a 32 byte hash value/);
     }
   });
 });
