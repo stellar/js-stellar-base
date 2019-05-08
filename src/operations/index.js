d12759d76486d98aa0fc06089d096d96084940f8
Morley Zhi <morley@stellar.org>
2019-05-08 16:41:11 -0400

    Restore deprecated functions Restore deprecated operations Operation.createPassiveOffer and Operation.manageOffer. When called, make them console.log an error, and return the correct new offer name.

--- src/operations/index.js
+++ src/operations/index.js
@@ -1,13 +1,35 @@
+import { manageSellOffer } from './manage_sell_offer';
+import { createPassiveSellOffer } from './create_passive_sell_offer';
+
 export { accountMerge } from './account_merge';
 export { allowTrust } from './allow_trust';
 export { bumpSequence } from './bump_sequence';
 export { changeTrust } from './change_trust';
 export { createAccount } from './create_account';
-export { createPassiveSellOffer } from './create_passive_sell_offer';
 export { inflation } from './inflation';
 export { manageData } from './manage_data';
-export { manageSellOffer } from './manage_sell_offer';
 export { manageBuyOffer } from './manage_buy_offer';
 export { pathPayment } from './path_payment';
 export { payment } from './payment';
 export { setOptions } from './set_options';
+
+export { manageSellOffer };
+export { createPassiveSellOffer };
+
+export function manageOffer(opts) {
+  // eslint-disable-next-line no-console
+  console.log(
+    '[Operation] Operation.manageOffer has been renamed to Operation.manageSellOffer! The old name is deprecated and will be removed in a later version!'
+  );
+
+  return manageSellOffer.call(this, opts);
+}
+
+export function createPassiveOffer(opts) {
+  // eslint-disable-next-line no-console
+  console.log(
+    '[Operation] Operation.createPassiveOffer has been renamed to Operation.createPassiveSellOffer! The old name is deprecated and will be removed in a later version!'
+  );
+
+  return createPassiveSellOffer.call(this, opts);
+}
