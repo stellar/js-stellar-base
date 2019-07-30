fbb10d059319ba0111d280ad76d17241ef8992ff
Adolfo Builes <builes.adolfo@gmail.com>
2019-07-30 14:48:30 -0500

    Typings for js xdr memo network (#219)
    * Add typings for network.
    
    * Add typings for memo.
    
    * Add @types/js-xdr.
    
    * Add TS-TODO.

---
+++ src/@types/js-xdr/index.d.ts
@@ -0,0 +1,42 @@
+// Type definitions for js-xdr v1.1.1
+declare module "js-xdr" {
+  // TS-TODO: Add original signature https://github.com/stellar/js-stellar-base/blob/typescript/types/index.d.ts#L530
+  export class Operation extends XDRStruct {
+    static fromXDR(xdr: Buffer): Operation;
+  }
+
+  export class XDRStruct {
+    static fromXDR(xdr: Buffer): XDRStruct;
+
+    toXDR(base?: string): Buffer;
+    toXDR(encoding: string): string;
+  }
+
+  export class Asset extends XDRStruct {
+    static fromXDR(xdr: Buffer): Asset;
+  }
+
+  export class Memo extends XDRStruct {
+    static fromXDR(xdr: Buffer): Memo;
+  }
+
+  export class TransactionEnvelope extends XDRStruct {
+    static fromXDR(xdr: Buffer): TransactionEnvelope;
+  }
+
+  export class DecoratedSignature extends XDRStruct {
+    static fromXDR(xdr: Buffer): DecoratedSignature;
+
+    constructor(keys: { hint: SignatureHint; signature: Signature });
+
+    hint(): SignatureHint;
+    signature(): Buffer;
+  }
+
+  export type SignatureHint = Buffer;
+  export type Signature = Buffer;
+
+  export class TransactionResult extends XDRStruct {
+    static fromXDR(xdr: Buffer): TransactionResult;
+  }
+}
--- src/network.ts
+++ src/network.ts
@@ -1,17 +1,22 @@
 import { hash } from './hashing';
 
+interface Networks {
+  PUBLIC: string;
+  TESTNET: string;
+}
+
 /**
  * Contains passphrases for common networks:
  * * `Networks.PUBLIC`: `Public Global Stellar Network ; September 2015`
  * * `Networks.TESTNET`: `Test SDF Network ; September 2015`
  * @type {{PUBLIC: string, TESTNET: string}}
  */
-export const Networks = {
+export const Networks: Networks = {
   PUBLIC: 'Public Global Stellar Network ; September 2015',
   TESTNET: 'Test SDF Network ; September 2015'
 };
 
-let current = null;
+let current: Network | null = null;
 
 /**
  * The Network class provides helper methods to get the passphrase or id for different
@@ -26,7 +31,9 @@ let current = null;
  * @param {string} networkPassphrase Network passphrase
  */
 export class Network {
-  constructor(networkPassphrase) {
+  private _networkPassphrase: string;
+
+  constructor(networkPassphrase: string) {
     this._networkPassphrase = networkPassphrase;
   }
 
@@ -34,7 +41,7 @@ export class Network {
    * Use Stellar Public Network
    * @returns {void}
    */
-  static usePublicNetwork() {
+  static usePublicNetwork(): void {
     this.use(new Network(Networks.PUBLIC));
   }
 
@@ -42,7 +49,7 @@ export class Network {
    * Use test network.
    * @returns {void}
    */
-  static useTestNetwork() {
+  static useTestNetwork(): void {
     this.use(new Network(Networks.TESTNET));
   }
 
@@ -51,28 +58,28 @@ export class Network {
    * @param {Network} network Network to use
    * @returns {void}
    */
-  static use(network) {
+  static use(network: Network): void {
     current = network;
   }
 
   /**
    * @returns {Network} Currently selected network
    */
-  static current() {
+  static current(): Network | null {
     return current;
   }
 
   /**
    * @returns {string} Network passphrase
    */
-  networkPassphrase() {
+  public networkPassphrase(): string {
     return this._networkPassphrase;
   }
 
   /**
    * @returns {string} Network ID (SHA-256 hash of network passphrase)
    */
-  networkId() {
+  public networkId(): Buffer {
     return hash(this.networkPassphrase());
   }
 }
