dfa5c71b3adb206e3273c80c33a3aff71d210044
Akuukis <Akuukis@users.noreply.github.com>
2019-08-09 22:48:51 +0300

    fix(typescript): hashing (#240)

--- src/hashing.ts
+++ src/hashing.ts
@@ -1,7 +1,13 @@
 import { sha256 } from 'sha.js';
 
-export function hash(data) {
+type BufferAlikeES2015 = DataView | Uint8Array | Uint8ClampedArray | Uint16Array | Uint32Array | Int8Array | Int16Array | Int32Array | Float32Array | Float64Array
+
+export function hash(data: string | Buffer | BufferAlikeES2015): Buffer {
   const hasher = new sha256();
-  hasher.update(data, 'utf8');
+  if(typeof data === 'string') {
+    hasher.update(data, 'utf8');
+  } else {
+    hasher.update(data)
+  }
   return hasher.digest();
 }
