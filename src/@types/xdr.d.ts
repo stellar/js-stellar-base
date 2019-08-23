d8635192f546b5ef45dc2c798844a2834be1375c
Akuukis <akuukis@kalvis.lv>
2019-08-23 14:21:29 +0300

    fix(typescript): type XDR.config()

--- src/@types/xdr.d.ts
+++ src/@types/xdr.d.ts
@@ -6,6 +6,41 @@
 declare module "js-xdr" {
     import Long from "long";
 
+    // interface IOMixin {
+    //     toXDR(format?: 'raw'): Buffer
+    //     toXDR(format: 'hex' | 'base64'): string
+    // }
+    // interface IOMixinBuilder<TInstance extends IOMixin> {
+    //     new (): TInstance;
+    //     toXDR(val: Buffer): Buffer
+    //     fromXDR(input: Buffer, format: 'raw'): TInstance
+    //     fromXDR(input: string, format?: 'hex' | 'base64'): TInstance
+    // }
+    export class IOMixin {
+        /**
+         * **REMEMBER TO REPLACE** static method `toXDR` to return the appropriate class.
+         */
+        static toXDR(val: Buffer): IOMixin
+        /**
+         * **REMEMBER TO REPLACE** static method `fromXDR` to return the appropriate class.
+         */
+        static fromXDR(input: Buffer, format: 'raw'): IOMixin
+        static fromXDR(input: string, format?: 'hex' | 'base64'): IOMixin
+    }
+
+    export class Bool extends IOMixin {
+    }
+    export class UnsignedInt extends IOMixin {
+    }
+    export class Float extends IOMixin {
+    }
+    export class Double extends IOMixin {
+    }
+    export class Quadruple extends IOMixin {
+    }
+    export class Int extends IOMixin {
+    }
+
     export class Array {
         public _childType: any;
         public _length: number;
@@ -13,8 +48,6 @@ declare module "js-xdr" {
 
     export class ChildStruct extends Struct {
     }
-
-    export function config(fn: any, types?: any): any;
 
     export class Enum {
         public name: string;
@@ -25,12 +58,12 @@ declare module "js-xdr" {
     export class Hyper extends Long {
     }
 
-    export class Opaque {
+    export class Opaque extends IOMixin {
+        new (length: number): Opaque;
     }
 
     export class Struct {
         public _attributes: object;
-        public toXDR(): Buffer;
     }
 
     export class String {
@@ -52,9 +85,68 @@ declare module "js-xdr" {
     }
 
     export class VarOpaque {
+        constructor(length?: number);
     }
 
     export class Option {
         public _childType: any;
     }
+
+    export class Void {
+    }
+
+}
+
+declare module "js-xdr" {  // `XDR.config`.
+
+    interface UnionConfigurationInt {
+        switchOn: Int,
+        switchName: string,
+        switches: [number, string | Void][],
+        arms: Record<string, Reference>
+    }
+    interface UnionConfigurationEnum {
+        switchOn: Reference,
+        switchName: string,
+        switches: [string, string | Void][],
+        arms: Record<string, Reference>
+        defaultArm?: IOMixin
+    }
+
+    class Reference {
+        resolve(context: unknown): void
+    }
+    class TypeBuilder {
+        constructor(destination: {})
+
+        enum(name: string, members: Record<string, number>): void
+        struct(name: string, members: [string, Reference | IOMixin][]): void
+        union(name: string, cfg: UnionConfigurationInt | UnionConfigurationEnum): void
+        typedef(name: string, cfg: Reference | IOMixin): void
+        const(name: string, cfg: number): void
+
+        void(): Void;
+        bool(): Bool;
+        int(): Int;
+        hyper(): Hyper;
+        uint(): UnsignedInt;
+        uhyper(): UnsignedHyper;
+        float(): Float;
+        double(): Double;
+        quadruple(): Quadruple;
+
+        string(length: number): Reference
+        opaque(length: number): Reference
+        varOpaque(length?: number): Reference
+
+        array(childType: IOMixin, length: number): Reference
+        varArray(childType: IOMixin, maxLength: Reference | number): Reference
+
+        option(childType: IOMixin): Reference
+
+        lookup(name: string): Reference
+    }
+
+    export function config(fn: (builder: TypeBuilder) => void, types?: any): any;
+
 }
