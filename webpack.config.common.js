327d721d7012568558a709dac8930d9cf732cb52
Tyler van der Hoeven <hi@tyvdh.com>
2019-12-12 09:12:36 -0500

    added a commonjs /dist output

---
+++ webpack.config.common.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+
+module.exports = {
+  output: {
+    libraryTarget: 'commonjs-module'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.js$/,
+        exclude: /node_modules\/(?!(crc)\/).*/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    // Ignore native modules (sodium-native)
+    new webpack.IgnorePlugin(/sodium-native/)
+  ]
+};
