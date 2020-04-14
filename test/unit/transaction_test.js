0dc1e0069a32a0a27d39014e52f63450ed3ef87b
Adolfo Builes <builes.adolfo@gmail.com>
2020-04-14 13:53:11 -0500

    Use builder to generate innerTx.

--- test/unit/transaction_test.js
+++ test/unit/transaction_test.js
@@ -461,31 +461,53 @@ describe('Transaction', function() {
 
   describe('FeeBumpTransaction', function() {
     it('handles fee bump transactions', function(done) {
-      let innerSource =
-        'GBZXN7PIRZGNMHGA7MUUUF4GWPY5AYPV6LY4UV2GL6VJGIQRXFDNMADI';
-      let destination =
-        'GDQERENWDDSQZS7R7WKHZI3BSOYMV3FSWR7TFUYFTKQ447PIX6NREOJM';
-      let amount = '2000.0000000';
-      let feeSource =
-        'GDQERENWDDSQZS7R7WKHZI3BSOYMV3FSWR7TFUYFTKQ447PIX6NREOJM';
       let baseFee = '100';
-      const xdr =
-        'AAAABQAAAADgSJG2GOUMy/H9lHyjYZOwyuyytH8y0wWaoc596L+bEgAAAAAAAADIAAAAAgAAAABzdv3ojkzWHMD7KUoXhrPx0GH18vHKV0ZfqpMiEblG1gAAAGQAAAAAAAAACAAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAA9IYXBweSBiaXJ0aGRheSEAAAAAAQAAAAAAAAABAAAAAOBIkbYY5QzL8f2UfKNhk7DK7LK0fzLTBZqhzn3ov5sSAAAAAAAAAASoF8gAAAAAAAAAAAERuUbWAAAAQK933Dnt1pxXlsf1B5CYn81PLxeYsx+MiV9EGbMdUfEcdDWUySyIkdzJefjpR5ejdXVp/KXosGmNUQ+DrIBlzg0AAAAAAAAAAei/mxIAAABAijIIQpL6KlFefiL4FP8UWQktWEz4wFgGNSaXe7mZdVMuiREntehi1b7MRqZ1h+W+Y0y+Z2HtMunsilT2yS5mAA==';
-      let transaction = new StellarBase.Transaction(
-        xdr,
-        'Standalone Network ; February 2017'
+      const networkPassphrase = 'Standalone Network ; February 2017';
+      const innerSource = StellarBase.Keypair.master(networkPassphrase);
+      const innerAccount = new StellarBase.Account(
+        innerSource.publicKey(),
+        '7'
       );
+      const destination =
+        'GDQERENWDDSQZS7R7WKHZI3BSOYMV3FSWR7TFUYFTKQ447PIX6NREOJM';
+      const amount = '2000.0000000';
+      const asset = StellarBase.Asset.native();
+
+      let innerTx = new StellarBase.TransactionBuilder(innerAccount, {
+        fee: '100',
+        networkPassphrase: networkPassphrase,
+        timebounds: {
+          minTime: 0,
+          maxTime: 0
+        },
+        v1: true
+      })
+        .addOperation(
+          StellarBase.Operation.payment({
+            destination,
+            asset,
+            amount
+          })
+        )
+        .addMemo(StellarBase.Memo.text('Happy birthday!'))
+        .build();
+      innerTx.sign(innerSource);
+
+      let feeSource = StellarBase.Keypair.fromSecret(
+        'SB7ZMPZB3YMMK5CUWENXVLZWBK4KYX4YU5JBXQNZSK2DP2Q7V3LVTO5V'
+      );
+      let transaction = StellarBase.TransactionBuilder.buildFeeBumpTransaction(
+        feeSource,
+        '100',
+        innerTx,
+        networkPassphrase
+      );
+      transaction.sign(feeSource);
 
       expect(transaction.isFeeBump()).to.equal(true);
-      expect(
-        transaction
-          .toEnvelope()
-          .toXDR()
-          .toString('base64')
-      ).to.be.equal(xdr);
 
-      expect(transaction.source).to.be.equal(innerSource);
-      expect(transaction.feeSource).to.be.equal(feeSource);
+      expect(transaction.source).to.be.equal(innerSource.publicKey());
+      expect(transaction.feeSource).to.be.equal(feeSource.publicKey());
 
       // shows new fee
       expect(transaction.fee).to.be.equal('200');
@@ -503,8 +525,17 @@ describe('Transaction', function() {
       expect(operation.destination).to.be.equal(destination);
       expect(operation.amount).to.be.equal(amount);
 
+      const expectedXDR =
+        'AAAABQAAAADgSJG2GOUMy/H9lHyjYZOwyuyytH8y0wWaoc596L+bEgAAAAAAAADIAAAAAgAAAABzdv3ojkzWHMD7KUoXhrPx0GH18vHKV0ZfqpMiEblG1gAAAGQAAAAAAAAACAAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAA9IYXBweSBiaXJ0aGRheSEAAAAAAQAAAAAAAAABAAAAAOBIkbYY5QzL8f2UfKNhk7DK7LK0fzLTBZqhzn3ov5sSAAAAAAAAAASoF8gAAAAAAAAAAAERuUbWAAAAQK933Dnt1pxXlsf1B5CYn81PLxeYsx+MiV9EGbMdUfEcdDWUySyIkdzJefjpR5ejdXVp/KXosGmNUQ+DrIBlzg0AAAAAAAAAAei/mxIAAABAijIIQpL6KlFefiL4FP8UWQktWEz4wFgGNSaXe7mZdVMuiREntehi1b7MRqZ1h+W+Y0y+Z2HtMunsilT2yS5mAA==';
+
+      expect(
+        transaction
+          .toEnvelope()
+          .toXDR()
+          .toString('base64')
+      ).to.be.equal(expectedXDR);
       let expectedTxEnvelope = StellarBase.xdr.TransactionEnvelope.fromXDR(
-        xdr,
+        expectedXDR,
         'base64'
       ).value();
 
