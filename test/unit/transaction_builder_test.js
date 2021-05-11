66745adf447a9544aeca612559b38953fd0284a3
George <Shaptic@users.noreply.github.com>
2021-05-11 11:16:55 -0700

    Stop trying to guess if ed25519 keys are actually med25519 keys. (#429)

--- test/unit/transaction_builder_test.js
+++ test/unit/transaction_builder_test.js
@@ -744,5 +744,14 @@ describe('TransactionBuilder', function() {
       expect(source.sequenceNumber()).to.equal('1235');
       expect(source.baseAccount().sequenceNumber()).to.equal('1235');
     });
+
+    it('does not regress js-stellar-sdk#646', function() {
+      expect(() => {
+        StellarBase.TransactionBuilder.fromXDR(
+          'AAAAAgAAAABg/GhKJU5ut52ih6Klx0ymGvsac1FPJig1CHYqyesIHQAAJxACBmMCAAAADgAAAAAAAAABAAAAATMAAAAAAAABAAAAAQAAAABg/GhKJU5ut52ih6Klx0ymGvsac1FPJig1CHYqyesIHQAAAAAAAAAAqdkSiA5dzNXstOtkPkHd6dAMPMA+MSXwK8OlrAGCKasAAAAAAcnDgAAAAAAAAAAByesIHQAAAEAuLrTfW6D+HYlUD9y+JolF1qrb40hIRATzsQaQjchKJuhOZJjLO0d7oaTD3JZ4UL4vVKtV7TvV17rQgCQnuz8F',
+          'Public Global Stellar Network ; September 2015'
+        );
+      }).to.not.throw();
+    });
   });
 });
