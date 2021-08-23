54e0070d772fc02745096530c06ddd998860123c
Marcelo Salloum <marcelo@stellar.org>
2021-08-23 15:12:33 -0300

    Added further tests for the `getLiquidityPoolId` method, based on the stellar-core tests https://github.com/stellar/stellar-core/blob/c5f6349b240818f716617ca6e0f08d295a6fad9a/src/transactions/test/LiquidityPoolTradeTests.cpp#L430-L526.

--- test/unit/liquidity_pool_id_test.js
+++ test/unit/liquidity_pool_id_test.js
@@ -69,6 +69,88 @@ describe('StellarBase#getLiquidityPoolId()', function() {
     );
   });
 
+  it('returns poolId correctly, based on stellar-core tests', function() {
+    // The tests below were extracted from https://github.com/stellar/stellar-core/blob/c5f6349b240818f716617ca6e0f08d295a6fad9a/src/transactions/test/LiquidityPoolTradeTests.cpp#L430-L526
+    const issuer1 = StellarBase.StrKey.encodeEd25519PublicKey(
+      Buffer.from(
+        '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef',
+        'hex'
+      )
+    );
+    const issuer2 = StellarBase.StrKey.encodeEd25519PublicKey(
+      Buffer.from(
+        'abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789',
+        'hex'
+      )
+    );
+
+    // NATIVE and ALPHANUM4 (short and full length)
+    let poolId = StellarBase.getLiquidityPoolId('constant_product', {
+      assetA: new StellarBase.Asset.native(),
+      assetB: new StellarBase.Asset('AbC', issuer1),
+      fee
+    });
+    expect(poolId.toString('hex')).to.equal(
+      'c17f36fbd210e43dca1cda8edc5b6c0f825fcb72b39f0392fd6309844d77ff7d'
+    );
+
+    poolId = StellarBase.getLiquidityPoolId('constant_product', {
+      assetA: new StellarBase.Asset.native(),
+      assetB: new StellarBase.Asset('AbCd', issuer1),
+      fee
+    });
+    expect(poolId.toString('hex')).to.equal(
+      '80e0c5dc79ed76bb7e63681f6456136762f0d01ede94bb379dbc793e66db35e6'
+    );
+
+    // NATIVE and ALPHANUM12 (short and full length)
+    poolId = StellarBase.getLiquidityPoolId('constant_product', {
+      assetA: new StellarBase.Asset.native(),
+      assetB: new StellarBase.Asset('AbCdEfGhIjK', issuer1),
+      fee
+    });
+    expect(poolId.toString('hex')).to.equal(
+      'd2306c6e8532f99418e9d38520865e1c1059cddb6793da3cc634224f2ffb5bd4'
+    );
+    poolId = StellarBase.getLiquidityPoolId('constant_product', {
+      assetA: new StellarBase.Asset.native(),
+      assetB: new StellarBase.Asset('AbCdEfGhIjKl', issuer1),
+      fee
+    });
+    expect(poolId.toString('hex')).to.equal(
+      '807e9e66653b5fda4dd4e672ff64a929fc5fdafe152eeadc07bb460c4849d711'
+    );
+
+    // ALPHANUM4 and ALPHANUM12: same code different issuer
+    poolId = StellarBase.getLiquidityPoolId('constant_product', {
+      assetA: new StellarBase.Asset('aBc', issuer1),
+      assetB: new StellarBase.Asset('aBc', issuer2),
+      fee
+    });
+    expect(poolId.toString('hex')).to.equal(
+      '5d7188454299529856586e81ea385d2c131c6afdd9d58c82e9aa558c16522fea'
+    );
+
+    poolId = StellarBase.getLiquidityPoolId('constant_product', {
+      assetA: new StellarBase.Asset('aBcDeFgHiJkL', issuer1),
+      assetB: new StellarBase.Asset('aBcDeFgHiJkL', issuer2),
+      fee
+    });
+    expect(poolId.toString('hex')).to.equal(
+      '93fa82ecaabe987461d1e3c8e0fd6510558b86ac82a41f7c70b112281be90c71'
+    );
+
+    // ALPHANUM4 before ALPHANUM12 doesn't depend on issuer or code
+    poolId = StellarBase.getLiquidityPoolId('constant_product', {
+      assetA: new StellarBase.Asset('aBc', issuer1),
+      assetB: new StellarBase.Asset('aBcDeFgHiJk', issuer2),
+      fee
+    });
+    expect(poolId.toString('hex')).to.equal(
+      'c0d4c87bbaade53764b904fde2901a0353af437e9d3a976f1252670b85a36895'
+    );
+  });
+
   it('throws an error if assets are not in lexicographic order', function() {
     expect(() =>
       StellarBase.getLiquidityPoolId('constant_product', {
