342a98758cee7584217107db02ece7624a9d4f54
George Kudrayvtsev <george@stellar.org>
2022-04-06 10:26:09 -0700

    Add docstrings all around

--- src/signerkey.js
+++ src/signerkey.js
@@ -1,7 +1,25 @@
 import xdr from './generated/stellar-xdr_generated';
 import { StrKey, encodeCheck, decodeCheck } from './strkey';
 
+/**
+ * A container class with helpers to convert between signer keys
+ * (`xdr.SignerKey`) and {@link StrKey}s.
+ *
+ * It's primarly used for manipulating the `extraSigners` precondition on a
+ * {@link Transaction}.
+ *
+ * @see {@link TransactionBuilder.setExtraSigners}
+ */
 export class SignerKey {
+  /**
+   * Decodes a StrKey address into an xdr.SignerKey instance.
+   *
+   * Only ED25519 public keys (G...), pre-auth transactions (T...), hashes
+   * (H...), and signed payloads (P...) can be signer keys.
+   *
+   * @param   {string} address  a StrKey-encoded signer address
+   * @returns {xdr.SignerKey}
+   */
   static decodeAddress(address) {
     const signerKeyMap = {
       ed25519PublicKey: xdr.SignerKey.signerKeyTypeEd25519,
@@ -34,9 +52,14 @@ export class SignerKey {
     }
   }
 
+  /**
+   * Encodes a signer key into its StrKey equivalent.
+   *
+   * @param   {xdr.SignerKey} signerKey   the signer
+   * @returns {string} the StrKey representation of the signer
+   */
   static encodeSignerKey(signerKey) {
-    let strkeyType;
-    let raw;
+    let strkeyType, raw;
 
     switch (signerKey.switch()) {
       case xdr.SignerKeyType.signerKeyTypeEd25519():
