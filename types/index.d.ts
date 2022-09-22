608573f8ca8c85d67dd952397bb020ce7e9bfe71
Paul Bellamy <paul@stellar.org>
2022-09-22 16:36:38 +0100

    Add FUTURENET network passphrase

--- types/index.d.ts
+++ types/index.d.ts
@@ -226,6 +226,7 @@ export class Memo<T extends MemoType = MemoType> {
 export enum Networks {
   PUBLIC = 'Public Global Stellar Network ; September 2015',
   TESTNET = 'Test SDF Network ; September 2015',
+  FUTURENET = 'Test SDF Future Network ; October 2022',
   SANDBOX = 'Local Sandbox Stellar Network ; September 2022'
 }
 
