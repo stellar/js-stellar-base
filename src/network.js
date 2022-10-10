91bcf3cf1eea69fc01629b8fe887186a6b3dd9f6
Paul Bellamy <paul@stellar.org>
2022-10-10 07:53:12 -0700

    Include standalone network passphrase (#555)
    * Add STANDALONE network passphrase
    
    * bump version

--- src/network.js
+++ src/network.js
@@ -8,5 +8,6 @@ export const Networks = {
   PUBLIC: 'Public Global Stellar Network ; September 2015',
   TESTNET: 'Test SDF Network ; September 2015',
   FUTURENET: 'Test SDF Future Network ; October 2022',
-  SANDBOX: 'Local Sandbox Stellar Network ; September 2022'
+  SANDBOX: 'Local Sandbox Stellar Network ; September 2022',
+  STANDALONE: 'Standalone Network ; February 2017'
 };
