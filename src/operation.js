0f68137745a35078702681efaf47a41b5d89cfba
Paul Bellamy <paul@stellar.org>
2022-12-08 19:32:24 +0000

    New soroban xdr updates (#566)
    * Missed removing parameters from the operation builder
    
    * Fix bug in contract.call footprint
    
    * Fixing up tests

--- src/operation.js
+++ src/operation.js
@@ -383,7 +383,6 @@ export class Operation {
       case 'invokeHostFunction': {
         result.type = 'invokeHostFunction';
         result.function = attrs.function();
-        result.parameters = attrs.parameters();
         result.footprint = attrs.footprint();
         break;
       }
--- test/unit/contract_test.js
+++ test/unit/contract_test.js
@@ -1,17 +1,21 @@
 describe('Contract.call', function() {
   it('includes the contract code footprint', function() {
-    let contractId = '00000000000000000000000000000001';
+    let contractId =
+      '0000000000000000000000000000000000000000000000000000000000000001';
     let contract = new StellarBase.Contract(contractId);
+    expect(contract.contractId()).to.equal(contractId);
     let call = contract.call('foo');
     let op = call.body().invokeHostFunctionOp();
     let readOnly = op.footprint().readOnly();
     expect(readOnly.length).to.equal(1);
-    let expected = new StellarBase.xdr.LedgerKeyContractData({
-      contractId,
-      key: StellarBase.xdr.ScVal.scvStatic(
-        StellarBase.xdr.ScStatic.scsLedgerKeyContractCode()
-      )
-    })
+    let expected = new StellarBase.xdr.LedgerKey.contractData(
+      new StellarBase.xdr.LedgerKeyContractData({
+        contractId: Buffer.from(contractId, 'hex'),
+        key: StellarBase.xdr.ScVal.scvStatic(
+          StellarBase.xdr.ScStatic.scsLedgerKeyContractCode()
+        )
+      })
+    )
       .toXDR()
       .toString('base64');
     expect(readOnly[0].toXDR().toString('base64')).to.equal(expected);
