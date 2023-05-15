e87446d32113610dbccd42213b3ed50e71d4157e
George Kudrayvtsev <george@stellar.org>
2023-05-15 14:08:45 -0700

    Bump all dependencies to their latest versions

--- test/unit/asset_test.js
+++ test/unit/asset_test.js
@@ -367,8 +367,14 @@ describe('Asset', function () {
 
     it('test if codes with upper-case letters are sorted before lower-case letters', function () {
       // All upper-case letters should come before any lower-case ones
-      const assetA = new StellarBase.Asset('B', 'GA7NLOF4EHWMJF6DBXXV2H6AYI7IHYWNFZR6R52BYBLY7TE5Q74AIDRA');
-      const assetB = new StellarBase.Asset('a', 'GA7NLOF4EHWMJF6DBXXV2H6AYI7IHYWNFZR6R52BYBLY7TE5Q74AIDRA');
+      const assetA = new StellarBase.Asset(
+        'B',
+        'GA7NLOF4EHWMJF6DBXXV2H6AYI7IHYWNFZR6R52BYBLY7TE5Q74AIDRA'
+      );
+      const assetB = new StellarBase.Asset(
+        'a',
+        'GA7NLOF4EHWMJF6DBXXV2H6AYI7IHYWNFZR6R52BYBLY7TE5Q74AIDRA'
+      );
 
       expect(StellarBase.Asset.compare(assetA, assetB)).to.eq(-1);
     });
