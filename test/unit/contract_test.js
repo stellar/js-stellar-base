7eee405ad28b710d20c3d126ebab0a96052b6e30
Paul Bellamy <paul@stellar.org>
2023-05-30 15:56:45 +0100

    new Contract('C123...abc') support

--- test/unit/contract_test.js
+++ test/unit/contract_test.js
@@ -1,20 +1,42 @@
-describe('Contract.getFootprint', function () {
-  it('includes the correct contract code footprint', function () {
-    let contractId = '0'.repeat(63) + '1';
+describe('Contract', function () {
+  describe('constructor', function () {
+    it('parses strkeys', function () {
+      let contractId = 'CA3D5KRYM6CB7OWQ6TWYRR3Z4T7GNZLKERYNZGGA5SOAOPIFY6YQGAXE';
+      let contract = new StellarBase.Contract(contractId);
+      expect(contract.contractId('strkey')).to.equal(contractId);
+    });
 
-    let contract = new StellarBase.Contract(contractId);
-    expect(contract.contractId()).to.equal(contractId);
+    it('parses hex addresses', function () {
+      let contractId = '0'.repeat(63) + '1';
+      let contract = new StellarBase.Contract(contractId);
+      expect(contract.contractId('hex')).to.equal(contractId);
+    });
 
-    const fp = contract.getFootprint();
+    it('parses throws on invalid ids', function () {
+      expect(() => {
+        new StellarBase.Contract('foobar')
+      }).to.throw();
+    });
+  });
 
-    let expected = new StellarBase.xdr.LedgerKey.contractData(
-      new StellarBase.xdr.LedgerKeyContractData({
-        contractId: Buffer.from(contractId, 'hex'),
-        key: StellarBase.xdr.ScVal.scvLedgerKeyContractExecutable()
-      })
-    )
-      .toXDR()
-      .toString('base64');
-    expect(fp.toXDR().toString('base64')).to.equal(expected);
+  describe('getFootprint', function () {
+    it('includes the correct contract code footprint', function () {
+      let contractId = '0'.repeat(63) + '1';
+
+      let contract = new StellarBase.Contract(contractId);
+      expect(contract.contractId('hex')).to.equal(contractId);
+
+      const fp = contract.getFootprint();
+
+      let expected = new StellarBase.xdr.LedgerKey.contractData(
+        new StellarBase.xdr.LedgerKeyContractData({
+          contractId: Buffer.from(contractId, 'hex'),
+          key: StellarBase.xdr.ScVal.scvLedgerKeyContractExecutable()
+        })
+      )
+        .toXDR()
+        .toString('base64');
+      expect(fp.toXDR().toString('base64')).to.equal(expected);
+    });
   });
 });
