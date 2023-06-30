a992b0e7ec08ea992040a12ccb6c97c37782ae24
George Kudrayvtsev <george@stellar.org>
2023-06-30 12:53:16 -0700

    Remove browser test (it was doing nothing??)

--- test/unit/browser_test.js
+++
@@ -1,6 +0,0 @@
-describe('Browser version tests', function () {
-  it('window object exists', function () {
-    // Test passes if window is defined
-    expect(typeof window).to.not.equal('undefined');
-  });
-});
