2659c714e985ea2d88414763ad18550bb5c70cda
Jeesun Kim <jeesunikim@users.noreply.github.com>
2023-08-07 17:56:21 -0700

    add 'formatTokenAmount' and 'parseTokenAmount'

--- src/index.js
+++ src/index.js
@@ -1,48 +1,49 @@
 /* eslint-disable import/no-import-module-exports */
-import BigNumber from 'bignumber.js';
-import xdr from './xdr';
+import BigNumber from "bignumber.js";
+import xdr from "./xdr";
 
 BigNumber.DEBUG = true; // gives us exceptions on bad constructor values
 
 export { xdr };
-export { hash } from './hashing';
-export { sign, verify, FastSigning } from './signing';
+export { hash } from "./hashing";
+export { sign, verify, FastSigning } from "./signing";
 export {
   getLiquidityPoolId,
-  LiquidityPoolFeeV18
-} from './get_liquidity_pool_id';
-export { Keypair } from './keypair';
-export { UnsignedHyper, Hyper } from 'js-xdr';
-export { TransactionBase } from './transaction_base';
-export { Transaction } from './transaction';
-export { FeeBumpTransaction } from './fee_bump_transaction';
+  LiquidityPoolFeeV18,
+} from "./get_liquidity_pool_id";
+export { Keypair } from "./keypair";
+export { UnsignedHyper, Hyper } from "js-xdr";
+export { TransactionBase } from "./transaction_base";
+export { Transaction } from "./transaction";
+export { FeeBumpTransaction } from "./fee_bump_transaction";
 export {
   TransactionBuilder,
   TimeoutInfinite,
-  BASE_FEE
-} from './transaction_builder';
-export { Asset } from './asset';
-export { LiquidityPoolAsset } from './liquidity_pool_asset';
-export { LiquidityPoolId } from './liquidity_pool_id';
+  BASE_FEE,
+} from "./transaction_builder";
+export { Asset } from "./asset";
+export { LiquidityPoolAsset } from "./liquidity_pool_asset";
+export { LiquidityPoolId } from "./liquidity_pool_id";
 export {
   Operation,
   AuthRequiredFlag,
   AuthRevocableFlag,
   AuthImmutableFlag,
-  AuthClawbackEnabledFlag
-} from './operation';
-export * from './memo';
-export { Account } from './account';
-export { MuxedAccount } from './muxed_account';
-export { Claimant } from './claimant';
-export { Networks } from './network';
-export { StrKey } from './strkey';
-export { SignerKey } from './signerkey';
+  AuthClawbackEnabledFlag,
+} from "./operation";
+export * from "./memo";
+export { Account } from "./account";
+export { MuxedAccount } from "./muxed_account";
+export { Claimant } from "./claimant";
+export { Networks } from "./network";
+export { StrKey } from "./strkey";
+export { SignerKey } from "./signerkey";
+export { Soroban } from "./soroban";
 export {
   decodeAddressToMuxedAccount,
   encodeMuxedAccountToAddress,
   extractBaseAddress,
-  encodeMuxedAccount
-} from './util/decode_encode_muxed_account';
+  encodeMuxedAccount,
+} from "./util/decode_encode_muxed_account";
 
 export default module.exports;
---
+++ src/soroban.js
@@ -0,0 +1,74 @@
+import BigNumber from "bignumber.js";
+
+/**
+ * Soroban helper class
+ * formatting, parsing, and etc
+ * @class Soroban
+ */
+export class Soroban {
+  /**
+   * format token amount to its ideal specific number of decimals
+   *
+   * @param {BigNumber} amount - the token amount you want to display
+   * @param {number} decimals
+   * @returns {string}
+   */
+  static formatTokenAmount(amount, decimals) {
+    let formatted = amount.toString();
+
+    if (decimals > 0) {
+      formatted = amount.shiftedBy(-decimals).toFixed(decimals).toString();
+
+      // Trim trailing zeros
+      while (formatted[formatted.length - 1] === "0") {
+        formatted = formatted.substring(0, formatted.length - 1);
+      }
+
+      if (formatted.endsWith(".")) {
+        formatted = formatted.substring(0, formatted.length - 1);
+      }
+    }
+
+    return formatted;
+  }
+
+  /**
+   * parse token amount to use it on smart contract
+   *
+   * @param {string} value - the token amount you want to use it on smart contract
+   * @param {number} decimals
+   * @returns {string}
+   */
+  static parseTokenAmount(value, decimals) {
+    const comps = value.split(".");
+
+    let whole = comps[0];
+    let fraction = comps[1];
+    if (!whole) {
+      whole = "0";
+    }
+    if (!fraction) {
+      fraction = "0";
+    }
+
+    // Trim trailing zeros
+    while (fraction[fraction.length - 1] === "0") {
+      fraction = fraction.substring(0, fraction.length - 1);
+    }
+
+    // If decimals is 0, we have an empty string for fraction
+    if (fraction === "") {
+      fraction = "0";
+    }
+
+    // Fully pad the string with zeros to get to value
+    while (fraction.length < decimals) {
+      fraction += "0";
+    }
+
+    const wholeValue = new BigNumber(whole);
+    const fractionValue = new BigNumber(fraction);
+
+    return wholeValue.shiftedBy(decimals).plus(fractionValue);
+  }
+}
