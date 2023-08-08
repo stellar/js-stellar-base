2ad6569090ed755597859e24d1ab63e2098c21e4
Jeesun Kim <jeesunikim@users.noreply.github.com>
2023-08-08 09:48:45 -0700

    yarn fmt

--- src/soroban.js
+++ src/soroban.js
@@ -1,4 +1,4 @@
-import BigNumber from "bignumber.js";
+import BigNumber from 'bignumber.js';
 
 /**
  * Soroban helper class
@@ -20,11 +20,11 @@ export class Soroban {
       formatted = amount.shiftedBy(-decimals).toFixed(decimals).toString();
 
       // Trim trailing zeros
-      while (formatted[formatted.length - 1] === "0") {
+      while (formatted[formatted.length - 1] === '0') {
         formatted = formatted.substring(0, formatted.length - 1);
       }
 
-      if (formatted.endsWith(".")) {
+      if (formatted.endsWith('.')) {
         formatted = formatted.substring(0, formatted.length - 1);
       }
     }
@@ -40,30 +40,30 @@ export class Soroban {
    * @returns {string}
    */
   static parseTokenAmount(value, decimals) {
-    const comps = value.split(".");
+    const comps = value.split('.');
 
     let whole = comps[0];
     let fraction = comps[1];
     if (!whole) {
-      whole = "0";
+      whole = '0';
     }
     if (!fraction) {
-      fraction = "0";
+      fraction = '0';
     }
 
     // Trim trailing zeros
-    while (fraction[fraction.length - 1] === "0") {
+    while (fraction[fraction.length - 1] === '0') {
       fraction = fraction.substring(0, fraction.length - 1);
     }
 
     // If decimals is 0, we have an empty string for fraction
-    if (fraction === "") {
-      fraction = "0";
+    if (fraction === '') {
+      fraction = '0';
     }
 
     // Fully pad the string with zeros to get to value
     while (fraction.length < decimals) {
-      fraction += "0";
+      fraction += '0';
     }
 
     const wholeValue = new BigNumber(whole);
--- test/unit/soroban_test.js
+++ test/unit/soroban_test.js
@@ -1,12 +1,12 @@
-import BigNumber from "bignumber.js";
+import BigNumber from 'bignumber.js';
 
-describe("Soroban", function () {
-  describe("formatTokenAmount", function () {
+describe('Soroban', function () {
+  describe('formatTokenAmount', function () {
     it("returns '100.0000001' for '1000000001' of a token set to 7 decimals", function () {
       const amount = new BigNumber(1000000001);
 
       expect(StellarBase.Soroban.formatTokenAmount(amount, 7)).to.equal(
-        "100.0000001",
+        '100.0000001'
       );
     });
 
@@ -14,22 +14,22 @@ describe("Soroban", function () {
       const amount = new BigNumber(10000000010);
 
       expect(StellarBase.Soroban.formatTokenAmount(amount, 5)).to.equal(
-        "100000.0001",
+        '100000.0001'
       );
     });
 
     it("returns '10000.00001' for '1000000001.'.' of a token set to 5 decimals", function () {
-      const amount = new BigNumber("1000000001.");
+      const amount = new BigNumber('1000000001.');
 
       expect(StellarBase.Soroban.formatTokenAmount(amount, 5)).to.equal(
-        "10000.00001",
+        '10000.00001'
       );
     });
   });
 
-  describe("parseTokenAmount", function () {
+  describe('parseTokenAmount', function () {
     it("returns '10000' for '100' of a token set to 2 decimals", function () {
-      const amount = "100";
+      const amount = '100';
       const parsedAmount = StellarBase.Soroban.parseTokenAmount(amount, 2);
 
       expect(parsedAmount.toNumber()).to.equal(10000);
