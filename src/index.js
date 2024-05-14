7c029914972854502cd7834a89983045b47431d2
George <Shaptic@users.noreply.github.com>
2024-05-14 13:39:21 -0700

    Expose certain functions from `@stellar/js-xdr` so downstream don't need to mix dependencies. (#744)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 /* eslint-disable import/no-import-module-exports */
 import xdr from './xdr';
+import cereal from './jsxdr';
 
 export { xdr };
+export { cereal };
 export { hash } from './hashing';
 export { sign, verify, FastSigning } from './signing';
 export {
