9402f35ccf2c1002bb4937df2111852dfcf08c73
George <george@stellar.org>
2025-01-16 09:47:29 -0800

    Add tests+fixes for string/number cases

--- src/xdr.js
+++ src/xdr.js
@@ -2,24 +2,26 @@ import xdr from './generated/curr_generated';
 import { scValToNative } from './scval';
 
 xdr.scvMapSorted = (items) => {
-    return xdr.ScVal.scvMap(items.sort((a, b) => {
-        // Both a and b are `ScMapEntry`s, so we need to sort by underlying key.
-        //
-        // We couldn't possibly handle every combination of keys since Soroban
-        // maps don't enforce consistent types, so we do a best-effort and try
-        // sorting by "number-like" or "string-like."
-        let nativeA = scValToNative(a.key()),
-            nativeB = scValToNative(b.key());
+  let sorted = Array.from(items).sort((a, b) => {
+    // Both a and b are `ScMapEntry`s, so we need to sort by underlying key.
+    //
+    // We couldn't possibly handle every combination of keys since Soroban
+    // maps don't enforce consistent types, so we do a best-effort and try
+    // sorting by "number-like" or "string-like."
+    let nativeA = scValToNative(a.key()),
+      nativeB = scValToNative(b.key());
 
-        switch (typeof nativeA) {
-            case "number":
-            case "bigint":
-                return nativeA < nativeB;
+    switch (typeof nativeA) {
+      case 'number':
+      case 'bigint':
+        return nativeA < nativeB ? -1 : 1;
 
-            default:
-                return nativeA.toString().localeCompare(nativeB.toString());
-        }
-    }));
-}
+      default:
+        return nativeA.toString().localeCompare(nativeB.toString());
+    }
+  });
+
+  return xdr.ScVal.scvMap(sorted);
+};
 
 export default xdr;
