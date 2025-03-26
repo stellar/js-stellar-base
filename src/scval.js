7ac6cd82052532dda2e4535a2efad70be234fd63
George <george@stellar.org>
2025-03-26 10:42:36 -0700

    Silly linter, double quotes are for kids

--- src/scval.js
+++ src/scval.js
@@ -153,7 +153,7 @@ export function nativeToScVal(val, opts = {}) {
       }
 
       if (val instanceof Keypair) {
-        return nativeToScVal(val.publicKey(), { type: "address" });
+        return nativeToScVal(val.publicKey(), { type: 'address' });
       }
 
       if (val instanceof Contract) {
