e7bddf3b93c61caf6eb72bf93999828d9e5e0ef1
Ryan Yang <ryan.yang@stellar.org>
2026-02-10 10:53:35 -0800

    write manual type declaration files

---
+++ src/generated/curr_generated.d.ts
@@ -0,0 +1,4 @@
+// Links the generated XDR JavaScript to its type definitions.
+// The runtime `export default types` object matches the `xdr` namespace shape.
+export { xdr } from "../../types/curr";
+export { xdr as default } from "../../types/curr";
