0b16894ed78fb2facc97e07bdfb6968c4064144e
Ryan Yang <ryan.yang@stellar.org>
2026-02-10 10:46:08 -0800

    force relative imports in typescript src to use .js extension

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,3 +1,22 @@
+// Patch require to resolve .js imports to .ts files when the .js doesn't exist
+const Module = require("module");
+const originalResolveFilename = Module._resolveFilename;
+Module._resolveFilename = function (request, parent, ...args) {
+  try {
+    return originalResolveFilename.call(this, request, parent, ...args);
+  } catch (err) {
+    if (request.endsWith(".js")) {
+      return originalResolveFilename.call(
+        this,
+        request.replace(/\.js$/, ".ts"),
+        parent,
+        ...args
+      );
+    }
+    throw err;
+  }
+};
+
 if (typeof window === "undefined") {
   require("ts-node").register({
     project: "tsconfig.cjs.json",
