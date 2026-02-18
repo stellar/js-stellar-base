f9933f24fb93e3b765537cfa2ba6bbc91b48df01
Iveta <iveta@stellar.org>
2026-02-18 16:36:35 -0500

    src/soroban migrated + generated type declarations

---
+++ type_validation/network.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Contains passphrases for common networks:
+ * * `Networks.PUBLIC`: `Public Global Stellar Network ; September 2015`
+ * * `Networks.TESTNET`: `Test SDF Network ; September 2015`
+ * * `Networks.FUTURENET`: `Test SDF Future Network ; October 2022`
+ * * `Networks.SANDBOX`: `Local Sandbox Stellar Network ; September 2022`
+ * * `Networks.STANDALONE`: `Standalone Network ; February 2017`
+ */
+export declare const Networks: {
+    readonly PUBLIC: "Public Global Stellar Network ; September 2015";
+    readonly TESTNET: "Test SDF Network ; September 2015";
+    readonly FUTURENET: "Test SDF Future Network ; October 2022";
+    readonly SANDBOX: "Local Sandbox Stellar Network ; September 2022";
+    readonly STANDALONE: "Standalone Network ; February 2017";
+};
