ad9ec4d6e4bd68fec9699254bdc1b0dee0552d83
Iveta <quietbits@users.noreply.github.com>
2026-03-06 13:17:26 -0500

    Migrated `liquidity_pool_asset.js` file to TS (#915)
    * Migrated liquidity_pool_asset.js file to TS
    
    * Use AssetType from Asset

--- src/liquidity_pool_asset.ts
+++ src/liquidity_pool_asset.ts
@@ -1,29 +1,36 @@
-import xdr from "./xdr";
-import { Asset } from "./asset";
+import xdr from "./xdr.js";
+import { Asset } from "./asset.js";
 import {
   LiquidityPoolFeeV18,
-  getLiquidityPoolId
-} from "./get_liquidity_pool_id";
+  getLiquidityPoolId,
+  LiquidityPoolParameters
+} from "./get_liquidity_pool_id.js";
 
 /**
  * LiquidityPoolAsset class represents a liquidity pool trustline change.
  *
- * @constructor
- * @param {Asset} assetA – The first asset in the Pool, it must respect the rule assetA < assetB. See {@link Asset.compare} for more details on how assets are sorted.
- * @param {Asset} assetB – The second asset in the Pool, it must respect the rule assetA < assetB. See {@link Asset.compare} for more details on how assets are sorted.
- * @param {number} fee – The liquidity pool fee. For now the only fee supported is `30`.
+ * @param assetA – The first asset in the Pool, it must respect the rule assetA < assetB. See {@link Asset.compare} for more details on how assets are sorted.
+ * @param assetB – The second asset in the Pool, it must respect the rule assetA < assetB. See {@link Asset.compare} for more details on how assets are sorted.
+ * @param fee – The liquidity pool fee. For now the only fee supported is `30`.
  */
 export class LiquidityPoolAsset {
-  constructor(assetA, assetB, fee) {
+  assetA: Asset;
+  assetB: Asset;
+  fee: number;
+
+  constructor(assetA: Asset, assetB: Asset, fee: number) {
     if (!assetA || !(assetA instanceof Asset)) {
       throw new Error("assetA is invalid");
     }
+
     if (!assetB || !(assetB instanceof Asset)) {
       throw new Error("assetB is invalid");
     }
+
     if (Asset.compare(assetA, assetB) !== -1) {
       throw new Error("Assets are not in lexicographic order");
     }
+
     if (!fee || fee !== LiquidityPoolFeeV18) {
       throw new Error("fee is invalid");
     }
@@ -36,15 +43,17 @@ export class LiquidityPoolAsset {
   /**
    * Returns a liquidity pool asset object from its XDR ChangeTrustAsset object
    * representation.
-   * @param {xdr.ChangeTrustAsset} ctAssetXdr - The asset XDR object.
-   * @returns {LiquidityPoolAsset}
+   *
+   * @param ctAssetXdr - The asset XDR object.
    */
-  static fromOperation(ctAssetXdr) {
+  static fromOperation(ctAssetXdr: xdr.ChangeTrustAsset): LiquidityPoolAsset {
     const assetType = ctAssetXdr.switch();
+
     if (assetType === xdr.AssetType.assetTypePoolShare()) {
       const liquidityPoolParameters = ctAssetXdr
         .liquidityPool()
         .constantProduct();
+
       return new this(
         Asset.fromOperation(liquidityPoolParameters.assetA()),
         Asset.fromOperation(liquidityPoolParameters.assetB()),
@@ -61,27 +70,26 @@ export class LiquidityPoolAsset {
    * Note: To convert from an {@link Asset `Asset`} to `xdr.ChangeTrustAsset`
    * please refer to the
    * {@link Asset.toChangeTrustXDRObject `Asset.toChangeTrustXDRObject`} method.
-   *
-   * @returns {xdr.ChangeTrustAsset} XDR ChangeTrustAsset object.
    */
-  toXDRObject() {
+  toXDRObject(): xdr.ChangeTrustAsset {
     const lpConstantProductParamsXdr =
       new xdr.LiquidityPoolConstantProductParameters({
         assetA: this.assetA.toXDRObject(),
         assetB: this.assetB.toXDRObject(),
         fee: this.fee
       });
-    const lpParamsXdr = new xdr.LiquidityPoolParameters(
-      "liquidityPoolConstantProduct",
-      lpConstantProductParamsXdr
-    );
-    return new xdr.ChangeTrustAsset("assetTypePoolShare", lpParamsXdr);
+    const lpParamsXdr =
+      xdr.LiquidityPoolParameters.liquidityPoolConstantProduct(
+        lpConstantProductParamsXdr
+      );
+
+    return xdr.ChangeTrustAsset.assetTypePoolShare(lpParamsXdr);
   }
 
   /**
-   * @returns {LiquidityPoolParameters} Liquidity pool parameters.
+   * Returns liquidity pool parameters.
    */
-  getLiquidityPoolParameters() {
+  getLiquidityPoolParameters(): LiquidityPoolParameters {
     return {
       ...this,
       assetA: this.assetA,
@@ -92,17 +100,15 @@ export class LiquidityPoolAsset {
 
   /**
    * @see [Assets concept](https://developers.stellar.org/docs/glossary/assets/)
-   * @returns {AssetType.liquidityPoolShares} asset type. Can only be `liquidity_pool_shares`.
    */
-  getAssetType() {
+  getAssetType(): "liquidity_pool_shares" {
     return "liquidity_pool_shares";
   }
 
   /**
-   * @param {LiquidityPoolAsset} other the LiquidityPoolAsset to compare
-   * @returns {boolean} `true` if this asset equals the given asset.
+   * @param other - the LiquidityPoolAsset to compare
    */
-  equals(other) {
+  equals(other: LiquidityPoolAsset): boolean {
     return (
       this.assetA.equals(other.assetA) &&
       this.assetB.equals(other.assetB) &&
@@ -110,11 +116,12 @@ export class LiquidityPoolAsset {
     );
   }
 
-  toString() {
+  toString(): string {
     const poolId = getLiquidityPoolId(
       "constant_product",
       this.getLiquidityPoolParameters()
     ).toString("hex");
+
     return `liquidity_pool:${poolId}`;
   }
 }
