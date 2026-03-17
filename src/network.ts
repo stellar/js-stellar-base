e353cd75448b3cfa936b92ed450975824b049431
Iveta <iveta@stellar.org>
2026-03-17 11:16:01 -0400

    network updated

--- src/network.ts
+++ src/network.ts
@@ -6,10 +6,10 @@
  * * `Networks.SANDBOX`: `Local Sandbox Stellar Network ; September 2022`
  * * `Networks.STANDALONE`: `Standalone Network ; February 2017`
  */
-export const Networks = {
-  PUBLIC: "Public Global Stellar Network ; September 2015",
-  TESTNET: "Test SDF Network ; September 2015",
-  FUTURENET: "Test SDF Future Network ; October 2022",
-  SANDBOX: "Local Sandbox Stellar Network ; September 2022",
-  STANDALONE: "Standalone Network ; February 2017",
-} as const;
+export enum Networks {
+  PUBLIC = "Public Global Stellar Network ; September 2015",
+  TESTNET = "Test SDF Network ; September 2015",
+  FUTURENET = "Test SDF Future Network ; October 2022",
+  SANDBOX = "Local Sandbox Stellar Network ; September 2022",
+  STANDALONE = "Standalone Network ; February 2017"
+}
