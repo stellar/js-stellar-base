e54935a754ddfe17d512c27665a7e22b7750e05c
Iveta <iveta@stellar.org>
2026-03-17 10:52:03 -0400

    liquidity_pool_asset updated

--- type_validation/liquidity_pool_asset.d.ts
+++ type_validation/liquidity_pool_asset.d.ts
@@ -3,15 +3,16 @@ import { Asset } from "./asset.js";
 import { LiquidityPoolParameters } from "./get_liquidity_pool_id.js";
 /**
  * LiquidityPoolAsset class represents a liquidity pool trustline change.
- *
- * @param assetA – The first asset in the Pool, it must respect the rule assetA < assetB. See {@link Asset.compare} for more details on how assets are sorted.
- * @param assetB – The second asset in the Pool, it must respect the rule assetA < assetB. See {@link Asset.compare} for more details on how assets are sorted.
- * @param fee – The liquidity pool fee. For now the only fee supported is `30`.
  */
 export declare class LiquidityPoolAsset {
     assetA: Asset;
     assetB: Asset;
     fee: number;
+    /**
+     * @param assetA - The first asset in the Pool, it must respect the rule assetA < assetB. See {@link Asset.compare} for more details on how assets are sorted.
+     * @param assetB - The second asset in the Pool, it must respect the rule assetA < assetB. See {@link Asset.compare} for more details on how assets are sorted.
+     * @param fee - The liquidity pool fee. For now the only fee supported is `30`.
+     */
     constructor(assetA: Asset, assetB: Asset, fee: number);
     /**
      * Returns a liquidity pool asset object from its XDR ChangeTrustAsset object
@@ -33,12 +34,17 @@ export declare class LiquidityPoolAsset {
      */
     getLiquidityPoolParameters(): LiquidityPoolParameters;
     /**
+     * Returns the asset type, always `"liquidity_pool_shares"`.
+     *
      * @see [Assets concept](https://developers.stellar.org/docs/glossary/assets/)
      */
     getAssetType(): "liquidity_pool_shares";
     /**
+     * Returns true if this liquidity pool asset equals the given one.
+     *
      * @param other - the LiquidityPoolAsset to compare
      */
     equals(other: LiquidityPoolAsset): boolean;
+    /** Returns a string representation in `liquidity_pool:<hex pool id>` format. */
     toString(): string;
 }
