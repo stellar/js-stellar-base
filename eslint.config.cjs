26097d1b3b131c48aeae52aa58065e6f02c9406a
Ryan Yang <ryan.yang@stellar.org>
2026-03-30 10:06:42 -0700

    ease eslint rules for test files

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -74,4 +74,17 @@ module.exports = eslintConfig.defineConfig(
       "import-x/extensions": ["error", "always", { ignorePackages: true }],
     },
   },
+  {
+    files: ["test/**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-return": "off",
+      "@typescript-eslint/unbound-method": "off",
+      "@typescript-eslint/require-await": "off",
+    },
+  },
 );
